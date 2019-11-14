7f285fb0df26d21f2cd41bdb6586df8d3f64750e
Ekain Susperregi Ibarguren <suspekain@gmail.com>
2019-11-14 19:15:25 +0100

    escoger equipos

--- js/equipos.js
+++ js/equipos.js
@@ -38,20 +38,46 @@ var teamH = '{"jugador" : [' +
   '{"nombre":"Cedric Digory", "posicion":"buscador", "puntos":0}' +
 ']}';
 
-var equipoGryffindor = JSON.parse(teamG);
-var equipoSlytherin = JSON.parse(teamS);
-var equipoRavenclaw = JSON.parse(teamR);
-var equipoHufflepuff = JSON.parse(teamH);
+var jugadoresGryffindor = JSON.parse(teamG);
+var jugadoresSlytherin = JSON.parse(teamS);
+var jugadoresRavenclaw = JSON.parse(teamR);
+var jugadoresHufflepuff = JSON.parse(teamH);
 
-for (var i = 0; i <= 6; i++) {
-  console.log(equipoGryffindor.jugador[i].nombre + " " + equipoGryffindor.jugador[i].posicion + " " + equipoGryffindor.jugador[i].puntos);
+class Equipo {
+  constructor(nombre,imagen,jugadores) {
+    this._nombre = nombre;
+    this._imagen = imagen;
+    this._jugadores = jugadores;
+    this._puntos = 0;
+  }
+
+  get nombre() {
+    return this._nombre;
+  }
+  set nombre(x) {
+    this._nombre = x;
+  }
+  get imagen() {
+    return this._imagen;
+  }
+  set imagen(x) {
+    this._imagen = x;
+  }
+  get jugadores() {
+    return this._jugadores;
+  }
+  set jugadores(x) {
+    this._jugadores = x;
+  }
+  get puntos() {
+    return this._puntos;
+  }
+  set puntos(x) {
+    this._puntos = x;
+  }
 }
-for (var i = 0; i <= 6; i++) {
-  console.log(equipoSlytherin.jugador[i].nombre + " " + equipoSlytherin.jugador[i].posicion + " " + equipoSlytherin.jugador[i].puntos);
-}
-for (var i = 0; i <= 6; i++) {
-  console.log(equipoRavenclaw.jugador[i].nombre + " " + equipoRavenclaw.jugador[i].posicion + " " + equipoRavenclaw.jugador[i].puntos);
-}
-for (var i = 0; i <= 6; i++) {
-  console.log(equipoHufflepuff.jugador[i].nombre + " " + equipoHufflepuff.jugador[i].posicion + " " + equipoHufflepuff.jugador[i].puntos);
-}
+
+gryffindor = new Equipo("gryffindor","gryffindor.png",jugadoresGryffindor);
+slytherin = new Equipo("gryffindor","gryffindor.png",jugadoresSlytherin);
+ravenclaw = new Equipo("gryffindor","gryffindor.png",jugadoresRavenclaw);
+hufflepuff = new Equipo("gryffindor","gryffindor.png",jugadoresHufflepuff);
---
+++ js/funciones.js
@@ -0,0 +1,6 @@
+var equipo = document.getElementById('equipos');
+
+function seleccionarEquipo(lado) {
+  equipo = equipo.options[equipos.selectedIndex].innerHTML;
+  console.log(equipo);
+}
